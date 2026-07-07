5def7b46dec5ac0c2c19a6e3982caf6b48851b2f
hiroaki106 <hiroaki.t106@gmail.com>
2026-07-08 00:12:17 +0900

    Add logging configuration example

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import logging
+
 from app.main import main
 
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s %(levelname)s %(name)s: %(message)s",
+    )
     main()
